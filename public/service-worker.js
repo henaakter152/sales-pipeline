5b9390864588bd76ab46235c04bac6d21dc5f39f
Hena Akter <henaakter12258@gmail.com>
2026-07-03 10:58:00 +0000

    feat: PWA + localStorage + APK-ready static app

---
+++ public/service-worker.js
@@ -0,0 +1,7 @@
+const CACHE = 'pipeline-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/sales-pipeline/'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
